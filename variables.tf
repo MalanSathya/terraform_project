f92af42bc97ad7ad8acce3a9c82fd15cd5c53947
MalanSathya <113691235+MalanSathya@users.noreply.github.com>
2025-03-11 10:03:29 -0400

    Update variables.tf
    Updated the common tags to display full name.

--- variables.tf
+++ variables.tf
@@ -38,7 +38,7 @@ variable "common_tags" {
   type = map(string)
   default = {
     Assignment     = "CCGC 5502 Automation Assignment"
-    Name           = "n01639496.lastname"
+    Name           = "Malan.VenkatesanSathyanaarayan"
     ExpirationDate = "2024-12-31"
     Environment    = "Learning"
   }
